1abfa803a63ec99f9dfd5028d2ea8e48accbf182
Wijayaac <wijayaac25@gmail.com>
2022-10-10 23:01:39 +0800

    Add : search function using debounce

--- src/components/Hotel/Filter/index.js
+++ src/components/Hotel/Filter/index.js
@@ -1,61 +1,82 @@
+import debounce from "lodash.debounce";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import React from 'react'
-import { set, useForm } from 'react-hook-form'
+import { getFilteredHotels } from "./Filter.handler";
+import style from "./style.module.css";
 
-
-import { getFilteredHotels } from './Filter.handler'
 const Filter = (props) => {
-    const { register, handleSubmit, reset } = useForm()
-    const { setHotels } = props
+  const { register, handleSubmit, reset, getValues, setValue } = useForm();
+  const { setHotels } = props;
 
-    function removeEmptyFields (data) {
-        Object.keys(data).forEach(key => {
-            if (data[key] === '' || data[key] == null) {
-                delete data[key]
-            }
-        })
+  function removeEmptyFields(data) {
+    Object.keys(data).forEach((key) => {
+      if (data[key] === "" || data[key] == null) {
+        delete data[key];
+      }
+    });
+  }
+
+  const onSubmit = (data) => {
+    removeEmptyFields(data);
+    if (data !== "") {
+      getFilteredHotels(data, setHotels);
     }
+  };
 
-    const onSubmit = (data) => {
-        removeEmptyFields(data)
-        if (data !== '') {
-            getFilteredHotels(data, setHotels)
-        }
-    }
-    const handleReset = () => {
-        reset()
-        getFilteredHotels('', setHotels)
-    }
+  const handleReset = () => {
+    reset();
+    getFilteredHotels("", setHotels);
+  };
 
+  const handleSearch = debounce((e) => {
+    setValue("name_like", e.target.value);
 
-    return (
-        <div className='filter'>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="field">
-                    <label htmlFor="location">Location</label>
-                    <select id="location" {...register('location')}>
-                        <option value="">All</option>
-                        <option value="Bandung">Bandung</option>
-                        <option value="Jakarta">Jakarta</option>
-                        <option value="Surabaya">Surabaya</option>
-                    </select>
-                </div>
-                <div className="field">
-                    <label htmlFor="stars">Rating</label>
-                    <select id="stars" {...register('stars')}>
-                        <option value="">All</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
-                </div>
-                <button>Apply</button>
-                <button type='button' onClick={handleReset}>Reset</button>
-            </form>
+    let filter = getValues();
+
+    removeEmptyFields(filter);
+
+    getFilteredHotels(filter, setHotels);
+  }, 500);
+
+  return (
+    <div className={style.filter}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={style.field}>
+          <label htmlFor='name_like'>Name</label>
+          <input
+            id='name_like'
+            {...register("name_like")}
+            onChange={handleSearch}
+          />
         </div>
-    )
-}
+        <div className={style.field}>
+          <label htmlFor='location'>Location</label>
+          <select id='location' {...register("location")}>
+            <option value=''>All</option>
+            <option value='Bandung'>Bandung</option>
+            <option value='Jakarta'>Jakarta</option>
+            <option value='Surabaya'>Surabaya</option>
+          </select>
+        </div>
+        <div className={style.field}>
+          <label htmlFor='stars'>Rating</label>
+          <select id='stars' {...register("stars")}>
+            <option value=''>All</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select>
+        </div>
+        <button>Apply</button>
+        <button type='button' onClick={handleReset}>
+          Reset
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Filter
+export default Filter;
